71ea7959408fd63af99af192775ed029cd398ff4
kantorn <kantorn.j@gmail.com>
2014-01-17 15:36:00 +0700

    test 15.35
    Conflicts:
    	AnimatedModel/AnimatedModelProject/Game.cs

--- AnimatedModel/AnimatedModelProject/Game.cs
+++ AnimatedModel/AnimatedModelProject/Game.cs
@@ -23,7 +23,7 @@ using WaveEngine.Common;
 using WaveEngine.Framework.Services;
 
 #endregion
-
+//test naja
 namespace AnimatedModelProject
 {
     public class Game : WaveEngine.Framework.Game
@@ -36,7 +36,7 @@ namespace AnimatedModelProject
             screenLayers.AddScene<MyScene>();
             screenLayers.Apply();
 
-            //testest naja 15.01
+            //test comment 15.05
         }
     }
 }
